1f5ac9fcd78afedb18048b582d2d760833812b41
Chandan K S <chandan@Chandans-MacBook-Pro.local>
2021-07-22 17:08:47 +0530

    django docker image

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ALLOWED_HOSTS = ['0.0.0.0']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'todo_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
